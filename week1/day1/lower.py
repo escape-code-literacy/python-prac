ad0e9698e1de9241703b4fa60ef72c4c4f0651bb
choo <choo3476@naver.com>
2022-04-25 22:27:21 +0900

    파일 상위 디렉토리 변경 및 대소문자 변환 내장함수 확인"

---
+++ week1/day1/lower.py
@@ -0,0 +1,4 @@
+string_object = "HELLOWORLD"
+real_string = string_object.lower()
+
+print(real_string)
---
+++ week1/day1/upper.py
@@ -0,0 +1,4 @@
+string_object = "helloworld"
+real_string = string_object.upper()
+
+print(real_string)
