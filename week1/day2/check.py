61d3b412b567a4d8548ef3a46a58b1af0ed3f20c
choo <choo3476@naver.com>
2022-05-09 21:05:12 +0900

    code spacing2

--- week1/day2/check.py
+++ week1/day2/check.py
@@ -3,9 +3,9 @@
 # 그래서 일정 숫자까진 자주 사용되는 객체를 하나 미리 생성해 놓는다.
 
 def check():
-    for x in range(-260,260):
-        for y in range(x,x+1):
-            if id(x)==id(y):
+    for x in range(-260, 260):
+        for y in range(x, x + 1):
+            if id(x) == id(y):
                 print("%d는 미리 생성된 객체" % x)
             else:
                 print("%d는 새롭게 만들어진 객체" % x)
