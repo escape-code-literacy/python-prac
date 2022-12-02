92ea958a11d43959fe7f7b54590320b1f9ba7958
내비SW국내품질팀/추정수 <1104618@device.autoever.com>
2022-12-02 12:57:53 +0900

    20221130_modified3_히힣

--- 2022/previous/prac.py
+++ 2022/previous/prac.py
@@ -4,15 +4,17 @@
     Solver : Choo
 """
 
+# TODO : 함수 구현하기
 def func(number):
-    # TO-DO : 함수 구현하기
     answer = True
-
+    
+    # Calculate sum of each digit
     a = str(number)
     result = 0
     for i in a:
         result = result + int(i)
 
+    # 소수 판별 : 2부터 자기 자신까지의 숫자 중 하나라도 나누어 떨어지면 False를 반환하도록 동작
     for j in range(2,result):
         if result % j == 0:
             answer = False
